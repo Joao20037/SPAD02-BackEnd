22abfb514053767166868e4a240c11d92b229091
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2024-06-25 19:47:31 -0300

    Route teste add

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 });
 
+//definindo rotas
+const routeTeste = require("./routes/routeTeste");
+
+// atribuindo rotas
+app.use('/test', routeTeste);
+
+// porta que vai ser usada
 const port = process.env.PORT || 3000;
-
-
+// ouvindo na porta
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
